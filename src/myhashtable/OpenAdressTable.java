2e9f2cf9db32424c668dc13247d8a905dc1827b5
Sidorina_Test1 <elena_petrova98@mail.ru>
2017-10-14 22:47:45 +0300

    Исправила все ваши замечания по коду, за исключением пункта 7, поскольку я пока не понимаю, как именно реализовать то, что Вы имели в виду. Дальше планирую реализацию файловый ввод/вывод и GUI-реализацию.

--- src/myhashtable/OpenAdressTable.java
+++ src/myhashtable/OpenAdressTable.java
@@ -1,17 +1,12 @@
 package myhashtable;
 
-import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public class OpenAdressTable<T1,T2> implements Map<T1,T2>, HFunction<T1> {
+public class OpenAdressTable<T1,T2> implements Map<T1,T2> {
 
     private Data<T1,T2> table[];
     private int sizeOfTable = 4;
-    private int freePlaces;
+    private int freePlaces, freeIndex;
     private int howEnlarge = 2; //на сколько увеличить таблицу
 
     public OpenAdressTable() {
@@ -31,14 +26,15 @@ public class OpenAdressTable<T1,T2> implements Map<T1,T2>, HFunction<T1> {
     
     private int keyIndex(T1 key, Data tab[]) {
         int hashIndex = hashFunction(key);
-        if (tab[hashIndex] == null)
-            return -1; //нет такого индекса
         int i = hashIndex;
+        freeIndex = -1;
         do {
             if (tab[i] == null) {
-                i=(i+1)%tab.length;
-                continue;
+                freeIndex = i;
+                return -1;
             }
+            if (tab[i].isFree() && freeIndex == -1)
+                freeIndex = i;
             if (!tab[i].isFree() && tab[i].getKey().equals(key))
                 return i;
             i=(i+1)%tab.length;
@@ -50,8 +46,8 @@ public class OpenAdressTable<T1,T2> implements Map<T1,T2>, HFunction<T1> {
     public T2 put(T1 key, T2 value) {
         T2 oldValue;
         oldValue = put(key, value, table);
-        if (freePlaces < 0) { //мест нет
-            sizeOfTable = table.length + howEnlarge; //увеличиваем таблицу
+        if (freeIndex == -1) { //мест нет
+            sizeOfTable = table.length * howEnlarge; //увеличиваем таблицу
             freePlaces = sizeOfTable; //создаем новую пустую увеличенную таблицу
             Data newTable[] = new Data[sizeOfTable];
             for (int i=0;i<table.length;i++) { //из старой таблицы пихаем все в новую
@@ -69,25 +65,27 @@ public class OpenAdressTable<T1,T2> implements Map<T1,T2>, HFunction<T1> {
         if (keyInd != -1) { //в таблице есть такой ключ
             T2 oldValue = (T2)tab[keyInd].getValue();
             table[keyInd].setValue(value);
+            freeIndex = keyInd;
             return oldValue;
         }
-        //такого ключа нет => ищем первое свободное место
-        int hashIndex = hashFunction(key);
-        int i = hashIndex;
-        do {
-            if (tab[i] == null) {
-                tab[i] = new Data(key, value);
-                freePlaces--;
-                return null;
-            }
-            if (tab[i].isFree()) {
-                tab[i].addData(key, value);
-                freePlaces--;
-                return null;
-            }
-            i = (i+1)%tab.length;
-        } while (i!=hashIndex);
-        freePlaces--;
+        
+        if (freeIndex == -1) {
+            freePlaces = 0;
+            return null;
+        }
+        
+        if (tab[freeIndex] == null) {
+            tab[freeIndex] = new Data(key, value);
+            freePlaces--;
+            return null;
+        }
+        
+        if (tab[freeIndex].isFree()) {
+            tab[freeIndex].addData(key, value);
+            freePlaces--;
+            return null;
+        }
+        
         return null;
     }
 
@@ -111,20 +109,18 @@ public class OpenAdressTable<T1,T2> implements Map<T1,T2>, HFunction<T1> {
 
     @Override
     public int size() {
-        return table.length;
+        return table.length - freePlaces;
     }
     
     @Override
     public boolean isEmpty() {
-        for (int i=0;i<table.length;i++) {
-            if (table[i] != null || !table[i].isFree())
-                return false;
-        }
-        return true;
+        return (size() == 0);
     }
 
     @Override
     public void clear() {
+        if (size() == 0)
+            return;
         for (int i=0;i<table.length;i++) {
             if (table[i] != null)
                 table[i].changeFreedom();
@@ -133,8 +129,8 @@ public class OpenAdressTable<T1,T2> implements Map<T1,T2>, HFunction<T1> {
     }
 
     @Override
-    public boolean containsKey(Object key) {
-        if (keyIndex((T1)key) == -1)
+    public boolean containsKey(Object key) { 
+        if (size() == 0 || keyIndex((T1)key) == -1)
             return false;
         return true;
     }
@@ -142,6 +138,8 @@ public class OpenAdressTable<T1,T2> implements Map<T1,T2>, HFunction<T1> {
     
     @Override
     public boolean containsValue(Object value) {
+        if (size() == 0)
+            return false;
        for (int i=0;i<table.length;i++) {
            if (table[i] == null || table[i].isFree())
                continue;
@@ -151,51 +149,46 @@ public class OpenAdressTable<T1,T2> implements Map<T1,T2>, HFunction<T1> {
        return false;
     }
     
-    @Override
-    public Set<T1> keySet() {
-        Set<T1> allKeys = new LinkedHashSet<>();
-        for (int i=0;i<table.length;i++) {
-            if (table[i] == null || table[i].isFree())
-                continue;
-            allKeys.add(table[i].getKey());
-        }
-        return allKeys;
-    }
+    @Override 
+    public Set<T1> keySet() { 
+        Set<T1> allKeys = new LinkedHashSet<>(); 
+        for (int i=0;i<table.length;i++) { 
+            if (table[i] == null || table[i].isFree()) 
+                continue; 
+            allKeys.add(table[i].getKey()); 
+        } 
+        return allKeys; 
+    } 
 
     
-    @Override
-    public Set<Entry<T1, T2>> entrySet() {
-        Set<Entry<T1,T2>> allData = new LinkedHashSet<>();
-        for (int i=0;i<table.length;i++) {
-            if (table[i] == null || table[i].isFree())
-                continue;
-            allData.add(new Data(table[i].getKey(), table[i].getValue()));
-        }
-        return allData;
+    @Override 
+    public Set<Entry<T1, T2>> entrySet() { 
+        Set<Entry<T1,T2>> allData = new LinkedHashSet<>(); 
+        for (int i=0;i<table.length;i++) { 
+            if (table[i] == null || table[i].isFree()) 
+                continue; 
+            allData.add(new Data(table[i].getKey(), table[i].getValue())); 
+            } 
+        return allData; 
     }
     
     @Override
     public void putAll(Map<? extends T1, ? extends T2> m) {
-        for (Map.Entry entry: m.entrySet()) {
-            Data<T1,T2> dat = (Data)entry;
-            T1 key = dat.getKey();
-            T2 value = dat.getValue();
-            put(key, value);
-        }
+        for (Map.Entry entry: m.entrySet()) 
+            put((T1)entry.getKey(), (T2)entry.getValue());
     }
     
-    @Override
-    public Collection<T2> values() {
-        List list = new LinkedList();
-        for (int i=0;i<table.length;i++) {
-            if (table[i] == null || table[i].isFree())
-                continue;
-            list.add(table[i].getValue());
-        }
-        return (Collection)list;
+    @Override 
+    public Collection<T2> values() { 
+       List list = new LinkedList(); 
+       for (int i=0;i<table.length;i++) { 
+           if (table[i] == null || table[i].isFree()) 
+              continue; 
+           list.add(table[i].getValue()); 
+       } 
+       return (Collection)list; 
     }
     
-    @Override
     public int hashFunction(T1 key) {
         int i = key.hashCode()%sizeOfTable; //индексы в диапазоне от 0 до размера таблицы-1
         return i;
@@ -217,5 +210,5 @@ public class OpenAdressTable<T1,T2> implements Map<T1,T2>, HFunction<T1> {
         }
         System.out.println("");            
     }
-    
+
 }
